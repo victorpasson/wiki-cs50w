5dae61050e92d79f645533b959d50dd8e13d749b
Joao Victor <victor.passon@yahoo.com.br>
2023-05-25 18:00:36 -0300

    Correct URL Bug

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entrie>", views.entries, name="entrie"),
     path("search/", views.search, name="search"),
-    path("/random", views.randompage, name="random"),
+    path("random/", views.randompage, name="random"),
     path("newpage/", views.newpage, name="newpage"),
     path("edit/<str:entrie>", views.edit, name="edit"),
 ]
