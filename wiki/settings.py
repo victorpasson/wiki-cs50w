4724693a5a5e8af5c1abb4ef9a95ce1fb44968a4
Joao Victor <victor.passon@yahoo.com.br>
2023-05-25 21:28:26 -0300

    Correct List

--- wiki/settings.py
+++ wiki/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # Configures the staticfiles directory to serve
 # static files from /static/ on our deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
